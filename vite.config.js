186b17d40712ef9f73f429ba0d1d9b8840ff2a69
meshachkebai <meshachkebai@outlook.com>
2025-11-27 12:04:24 +1000

    Fix base path for Cloudflare Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ads-dashboard/',
+  //base: '/ads-dashboard/',
   plugins: [
     react(),
     VitePWA({
